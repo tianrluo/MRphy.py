v0.1.4; - Fixed `toNumpy==False` bug in `asdict` methods in `mobjs`; - Fixed numerical precession issue in `utils.rfclamp`; - Fixed dimension problem in `utils.rf_r2c`; - Increased test coverages.

mrphy/mobjs.py
@@ -21,17 +21,32 @@ SpinCube = TypeVar('SpinCube', bound='SpinCube')
 
 class Pulse(object):
     """
-    # Attributes:
+        Pulse(;rf, gr, dt, gmax, smax, rfmax, desc, device, dtype)
+    *INPUTS*:
+    - `rf` (N,xy, nT,(nCoils)) "Gauss", `xy` for separating real and imag part.
+    - `gr` (N,xyz,nT) "Gauss/cm"
+    - `dt` (N,1,), "Sec" simulation temporal step size, i.e., dwell time.
+    - `gmax` (N, xyz|1) "Gauss/cm", max |gradient|.
+    - `smax` (N, xyz|1) "Gauss/cm/Sec", max |slew rate|.
+    - `rfmax` (N,(nCoils)) "Gauss", max |RF|.
+    - `desc` str, an description of the pulse to be constructed.
+    - `device` torch.device; `dtype` torch.dtype
+
+    *PROPERTIES*:
+    - `device`
+    - `dtype`
+    - `is_cuda`
+    - `shape` (N,1,nT)
+    - `gmax` (N, xyz|1) "Gauss/cm", max |gradient|.
+    - `smax` (N, xyz|1) "Gauss/cm/Sec", max |slew rate|.
+    - `rfmax` (N,(nCoils)) "Gauss", max |RF|.
     - `rf` (N,xy, nT,(nCoils)) "Gauss", `xy` for separating real and imag part.
     - `gr` (N,xyz,nT) "Gauss/cm"
     - `dt` (N,1,), "Sec" simulation temporal step size, i.e., dwell time.
     - `desc` str, an description of the pulse to be constructed.
-    - `gmax` (N, xyz|1) "Gauss/cm", max |gradient|.
-    - `smax` (N, xyz|1) "Gauss/cm/Sec", max |slew rate|.
-    - `rfmax` (N,(nCoils)) "Gauss", max |RF|.
     """
 
-    _readonly = ('device', 'dtype')
+    _readonly = ('device', 'dtype', 'is_cuda', 'shape')
     _limits = ('gmax', 'smax', 'rfmax')
     __slots__ = set(_readonly + _limits + ('rf', 'gr', 'dt', 'desc'))
 
@@ -52,24 +67,22 @@ class Pulse(object):
 
         super().__setattr__('device', device)
         super().__setattr__('dtype', dtype)
+        super().__setattr__('is_cuda', self.device.type == 'cuda')
 
         kw = {'device': self.device, 'dtype': self.dtype}
 
         if rf_miss:
             N, nT = gr.shape[0], gr.shape[2]
             rf = torch.zeros((N, 2, nT), **kw)
-
-        if gr_miss:
+        else:
             N, nT = rf.shape[0], rf.shape[2]
-            gr = torch.zeros((N, 3, nT), **kw)
+            if gr_miss:
+                gr = torch.zeros((N, 3, nT), **kw)
+            else:
+                assert (N == gr.shape[0] and nT == gr.shape[2])
+        super().__setattr__('shape', torch.Size((N, 1, nT)))
 
-        # super() here, as self.__setattr__() has interdependent sanity check.
-        rf, gr = rf.to(**kw), gr.to(**kw)
-        assert (rf.shape[0] == gr.shape[0] and rf.shape[2] == gr.shape[2])
-
-        super().__setattr__('rf', rf)
-        super().__setattr__('gr', gr)
-
+        self.rf, self.gr = rf.to(**kw), gr.to(**kw)
         self.dt, self.gmax, self.smax, self.rfmax = dt, gmax, smax, rfmax
         self.desc = desc
         return
@@ -81,12 +94,8 @@ class Pulse(object):
             kw = {'device': self.device, 'dtype': self.dtype}
             v = (v.to(**kw) if isinstance(v, Tensor) else tensor(v, **kw))
 
-        if (k == 'gr'):
-            shape = self.rf.shape
-            assert (v.shape[0] == shape[0] and v.shape[2] == shape[2])
-        if (k == 'rf'):
-            shape = self.gr.shape
-            assert (v.shape[0] == shape[0] and v.shape[2] == shape[2])
+        if k in ('rf', 'gr'):
+            assert(v.shape[0] == self.shape[0] and v.shape[2] == self.shape[2])
         if (k in ('gmax', 'smax')):
             v = v.expand(self.gr.shape[:2])
         if (k == 'rfmax' and v.ndim == 2 and v.shape[1] == 1):
@@ -97,8 +106,8 @@ class Pulse(object):
 
     def asdict(self, toNumpy: bool = True) -> dict:
         _ = ('rf', 'gr', 'dt', 'gmax', 'smax', 'rfmax')
-        fn_np = (lambda x: x.detach().cpu().numpy() if toNumpy else
-                 lambda x: x.detach())
+        fn_np = ((lambda x: x.detach().cpu().numpy()) if toNumpy else
+                 (lambda x: x.detach()))
 
         d = {k: fn_np(getattr(self, k)) for k in _}
         d.update({k: getattr(self, k) for k in ('desc', 'device', 'dtype')})
@@ -180,7 +189,7 @@ class SpinArray(object):
     `M_`, etc., avoiding repetitive allocations.
     """
 
-    _readonly = ('shape', 'mask', 'device', 'dtype', 'ndim', 'nM')
+    _readonly = ('shape', 'mask', 'device', 'dtype', 'is_cuda', 'ndim', 'nM')
     _compact = ('T1_', 'T2_', 'γ_', 'M_')
     __slots__ = set(_readonly + _compact)
 
@@ -207,6 +216,7 @@ class SpinArray(object):
         super().__setattr__('nM', torch.sum(mask).item())
         super().__setattr__('device', device)
         super().__setattr__('dtype', dtype)
+        super().__setattr__('is_cuda', self.device.type == 'cuda')
 
         assert((T1 is None) or (T1_ is None))
         if T1 is None:
@@ -301,8 +311,8 @@ class SpinArray(object):
         return M_
 
     def asdict(self, toNumpy: bool = True, doEmbed: bool = True) -> dict:
-        fn_np = (lambda x: x.detach().cpu().numpy() if toNumpy else
-                 lambda x: x.detach())
+        fn_np = ((lambda x: x.detach().cpu().numpy()) if toNumpy else
+                 (lambda x: x.detach()))
 
         _ = (('T1', 'T2', 'γ', 'M') if doEmbed else ('T1_', 'T2_', 'γ_', 'M_'))
         d = {k: fn_np(getattr(self, k)) for k in _}
@@ -557,8 +567,8 @@ class SpinCube(object):
                                          loc_=self.loc_, b1Map_=b1Map_)
 
     def asdict(self, toNumpy: bool = True, doEmbed: bool = True) -> dict:
-        fn_np = (lambda x: x.detach().cpu().numpy() if toNumpy else
-                 lambda x: x.detach())
+        fn_np = ((lambda x: x.detach().cpu().numpy()) if toNumpy else
+                 (lambda x: x.detach()))
 
         _ = (('loc', 'Δf') if doEmbed else ('loc', 'Δf'))
         d = {k: fn_np(getattr(self, k)) for k in _}
@@ -658,8 +668,9 @@ class Examples(object):
                         10*torch.atan(t - round(nT/2))/π], 1)  # (1,xyz,nT)
 
         # Pulse
-        print('Pulse(rf=rf, gr=gr, dt=gt, device=device, dtype=dtype)')
-        return Pulse(rf=rf, gr=gr, dt=dt, **kw)
+        p = Pulse(rf=rf, gr=gr, dt=dt, **kw)
+        print('Pulse(rf=rf, gr=gr, dt=gt, device=device, dtype=dtype): ')
+        return p
 
     @staticmethod
     def spincube() -> SpinCube:

mrphy/utils.py
@@ -1,11 +1,18 @@
-from typing import Tuple
+from typing import Tuple, Union
+from numbers import Number
 
-import numpy as np
-from numpy import ndarray
 import torch
+import numpy as np
+from numpy import ndarray as ndarray_c
 from torch import tensor, Tensor
 
 from mrphy import γH, dt0, π
+if torch.cuda.is_available():
+    import cupy as cp
+    from cupy import ndarray as ndarray_g
+    ndarrayA = Union[ndarray_c, ndarray_g]
+else:
+    ndarrayA = ndarray_c
 
 
 def ctrsub(shape):
@@ -85,7 +92,7 @@ def k2g(k: Tensor, isTx: bool,
     return g
 
 
-def rf_c2r(rf: ndarray) -> ndarray:
+def rf_c2r(rf: ndarrayA) -> ndarrayA:
     """
         rf_c2r(rf)
     *INPUTS*:
@@ -96,10 +103,13 @@ def rf_c2r(rf: ndarray) -> ndarray:
     See Also:
     `rf_r2c`
     """
-    return np.concatenate((np.real(rf), np.imag(rf)), axis=1)
+    if isinstance(rf, ndarray_c):
+        return np.concatenate((np.real(rf), np.imag(rf)), axis=1)
+    else:  # ndarray_g, i.e., cupy.ndarray
+        return cp.concatenate((cp.real(rf), cp.imag(rf)), axis=1)
 
 
-def rf_r2c(rf: ndarray) -> ndarray:
+def rf_r2c(rf: ndarrayA) -> ndarrayA:
     """
         rf_r2c(rf)
     *INPUTS*:
@@ -110,7 +120,7 @@ def rf_r2c(rf: ndarray) -> ndarray:
     See Also:
     `rf_c2r`
     """
-    return rf[:, 0, ...] + 1j*rf[:, 1, ...]
+    return rf[:, [0], ...] + 1j*rf[:, [1], ...]
 
 
 def rf2tρθ(rf: Tensor, rfmax: Tensor) -> Tuple[Tensor, Tensor]:
@@ -126,25 +136,29 @@ def rf2tρθ(rf: Tensor, rfmax: Tensor) -> Tuple[Tensor, Tensor]:
     See Also:
     `tρθ2rf`
     """
+    rfmax = rfmax[None] if rfmax.ndim == 0 else rfmax
     tρ = (rf.norm(dim=1, keepdim=True)/rfmax[:, None, None, ...]*π/2).tan()
     θ = torch.atan2(rf[:, [1], :], rf[:, [0], :])
     return tρ, θ
 
 
-def rfclamp(rf: Tensor, rfmax: Tensor) -> Tensor:
+def rfclamp(rf: Tensor, rfmax: Tensor, eps: Number = 1e-7) -> Tensor:
     """
         rfclamp(rf, rfmax)
     *INPUTS*:
     - `rf` (N, xy, nT, (nCoils)) RF pulse, Gauss, x for real, y for imag.
     - `rfmax` (N, (nCoils)) RF pulse, Gauss, x for real, y for imag.
+    *OPTIONALS*:
+    - `eps` effective `rfmax` is `rfmax-eps`, numerical precession.
     *OUTPUTS*:
     - `rf` (N, xy, nT, (nCoils)) |RF| clampled at rfmax
 
     See Also:
     `sclamp`
     """
+    rfmax = rfmax[None] if rfmax.ndim == 0 else rfmax
     rf_abs = rf.norm(dim=1, keepdim=True)
-    return rf.mul((rfmax[:, None, None, ...]/rf_abs).clamp_(max=1))
+    return rf.mul(((rfmax[:, None, None, ...]-eps)/rf_abs).clamp_(max=1))
 
 
 def s2g(s: Tensor, dt: Tensor = tensor([[dt0]])) -> Tensor:
@@ -193,6 +207,7 @@ def sclamp(s: Tensor, smax: Tensor) -> Tensor:
     See Also:
     `rfclamp`
     """
+    smax = (smax[None] if smax.ndim == 0 else smax).to(s)  # device & dtype
     return s.max(-smax[..., None]).min(smax[..., None])
 
 
@@ -224,6 +239,7 @@ def tρθ2rf(tρ: Tensor, θ: Tensor, rfmax: Tensor) -> Tensor:
     See Also:
     `rf2tρθ`
     """
+    rfmax = rfmax[None] if rfmax.ndim == 0 else rfmax
     rfmax = rfmax[:, None, None, ...]  # -> (N, 1, 1, (nCoils))
     return tρ.atan()/π*2*rfmax*torch.cat((θ.cos(), θ.sin()), dim=1)
 

tests/test_utils.py
@@ -3,11 +3,16 @@ import torch
 import pytest
 from torch import tensor, cuda
 
-from mrphy import γH, dt0
+from mrphy import γH, dt0, rfmax0, smax0
 from mrphy import utils
+if torch.cuda.is_available():
+    import cupy as cp
+
 
 # TODO:
 # unit_tests for other utils functions
+def to_np(x):
+    return x.detach().cpu().numpy()
 
 
 class Test_utils:
@@ -23,13 +28,13 @@ class Test_utils:
     γ = tensor([[γH]], device=device, dtype=dtype)  # Hz/Gauss
     dt = tensor([[dt0]], device=device, dtype=dtype)   # Sec
 
-    def test_ctrSub(self):
+    def test_ctrsub(self):
         dkw = self.dkw
         x = utils.ctrsub(torch.arange(7, **dkw)).cpu().numpy()
         assert(np.all(x == np.array([0, 0, 1, 1, 2, 2, 3])))
         return
 
-    def test_g2k(self):
+    def test_kgs(self):
 
         dkw, atol = self.dkw, self.atol
         γ, dt = self.γ, self.dt
@@ -37,23 +42,52 @@ class Test_utils:
         k = tensor([[[1., 2., 3., 4., 0.]]], **dkw)
         gTx, gRx = (utils.k2g(k, True, γ=γ, dt=dt),
                     utils.k2g(k, False, γ=γ, dt=dt))
+        gTx1 = utils.s2g(utils.g2s(gTx, dt), dt)
 
-        assert(utils.g2k(gTx, True, γ=γ, dt=dt).detach().cpu().numpy() ==
-               pytest.approx(k.detach().cpu().numpy(), abs=atol))
+        assert(to_np(utils.g2k(gTx, True, γ=γ, dt=dt)) ==
+               pytest.approx(to_np(k), abs=atol))
 
-        assert(utils.g2k(gRx, False, γ=γ, dt=dt).detach().cpu().numpy() ==
-               pytest.approx(k.detach().cpu().numpy(), abs=atol))
+        assert(to_np(utils.g2k(gRx, False, γ=γ, dt=dt)) ==
+               pytest.approx(to_np(k), abs=atol))
 
-        dt1 = tensor([1.], **dkw)
-        dt1_np, gTx_np = (dt1.detach().cpu().numpy(),
-                          gTx.detach().cpu().numpy())
-        assert(utils.g2s(gTx, dt=dt1).detach().cpu().numpy() ==
-               pytest.approx(np.concatenate((gTx_np[:, :, [0]],
-                             gTx_np[:, :, 1:] - gTx_np[:, :, :-1]),
-                             axis=2)/(dt1_np[..., None]),
-               abs=atol))
-
+        assert(to_np(gTx) == pytest.approx(to_np(gTx1), abs=atol))
         return
 
-    def test_g2s(self):
+    def test_rc_rf(self):
+        shape, atol = (1, 2, 5), self.atol
+        rf_r_0_np = np.random.rand(*shape).astype(np.double)
+        rf_r_1_np = utils.rf_c2r(utils.rf_r2c(rf_r_0_np))
+        assert(rf_r_0_np == pytest.approx(rf_r_1_np, abs=atol))
+
+        if torch.cuda.is_available():
+            rf_r_0_cp = cp.random.rand(*shape).astype(cp.double)
+            rf_r_1_cp = utils.rf_c2r(utils.rf_r2c(rf_r_0_cp))
+            assert(cp.asnumpy(rf_r_0_cp) ==
+                   pytest.approx(cp.asnumpy(rf_r_1_cp), abs=atol))
         return
+
+    def test_rfclamptan(self):
+        shape, rfmax, atol = (1, 2, 10), rfmax0, self.atol
+        rf0 = utils.rfclamp(rfmax0*((torch.rand(shape)-0.5)*4), rfmax)
+        assert(torch.all(rf0.norm(dim=1) <= rfmax))
+        tρ, θ = utils.rf2tρθ(rf0, rfmax)
+        rf1 = utils.tρθ2rf(tρ, θ, rfmax)
+        assert(to_np(rf0) == pytest.approx(to_np(rf1), abs=atol))
+        return
+
+    def test_sclamptan(self):
+        shape, smax, atol = (1, 3, 10), smax0, self.atol
+        s0 = utils.sclamp(smax0*((torch.rand(shape)-0.5)*4), smax)
+        assert(torch.all(s0.abs() <= smax))
+        s1 = utils.ts2s(utils.s2ts(s0, smax), smax)
+        assert(to_np(s0) == pytest.approx(to_np(s1), abs=atol))
+        return
+
+
+if __name__ == '__main__':
+    tmp = Test_utils()
+    tmp.test_ctrsub()
+    tmp.test_kgs()
+    tmp.test_rc_rf()
+    tmp.test_rfclamptan()
+    tmp.test_sclamptan()