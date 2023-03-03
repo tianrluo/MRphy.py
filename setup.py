645ffe15cb21cb9367757f6d2b74398bcf8c357d
ray@omni <tianrluo@users.noreply.github.com>
2023-03-02 21:29:46 -0500

    Bump up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("mrphy/version.py") as fp:
 __version__ = version['__version__']
 
 
-REQUIRED_PACKAGES = ['torch>=1.3', 'numpy', 'scipy']
+REQUIRED_PACKAGES = ['torch>=1.13', 'numpy', 'scipy']
 
 with open("README.md", "r") as h:
     long_description = h.read()
@@ -31,5 +31,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
